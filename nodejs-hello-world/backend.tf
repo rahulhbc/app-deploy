e4087ca646e29de042ecba46d4187524aa94d89d
jiyansh saini <rahulhbc@yahoo.in>
2025-03-24 00:40:16 +0530

    updated backened.tf

--- nodejs-hello-world/backend.tf
+++ nodejs-hello-world/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "RG-RBAC"
     storage_account_name = "storagelab001"
-    container_name       = "3tier"
-    key                  = "terraform.tfstate"
+    container_name       = "3tier-app"
+    key                  = "app-deploy.tfstate"
   }
 }
