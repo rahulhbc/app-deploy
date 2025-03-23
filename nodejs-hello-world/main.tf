b9e1741b0485fef4b9f81fabab76d54bd8b5c5aa
jiyansh saini <rahulhbc@yahoo.in>
2025-03-23 22:27:26 +0530

    handled issues for authentication key

--- nodejs-hello-world/main.tf
+++ nodejs-hello-world/main.tf
@@ -12,10 +12,17 @@ resource "azurerm_linux_virtual_machine" "frontend_vm" {
   disable_password_authentication = true
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("~/.ssh_keys/id_rsa.pub")
   }
 
   network_interface_ids = [module.tier3_app.frontend_nic_id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    disk_size_gb         = 8
+    name                 = "frontend-os-disk"
+  }
 
   source_image_reference {
     publisher = "Canonical"
@@ -29,14 +36,14 @@ resource "azurerm_linux_virtual_machine" "frontend_vm" {
       "sudo apt-get update -y",
       "sudo apt-get install -y docker.io -y",
       "sudo systemctl start docker",
-      "sudo docker run -d -p 80:3000 rahulhbc/nodejs-hello-world:v1"
+      "sudo docker run -d -p 3000:80 rahulhbc/nodejs-hello-world:v1"
     ]
 
     connection {
       type        = "ssh"
       host        = module.tier3_app.frontend_public_ip
       user        = "azureuser"
-      private_key = file("~/.ssh/id_rsa")
+      private_key = file("~/.ssh_keys/id_rsa")
     }
   }
 }
