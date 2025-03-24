ce8592aa27bf9bdcf085fde6ea4ca97c27b7d367
jiyansh saini <rahulhbc@yahoo.in>
2025-03-25 01:32:50 +0530

    changed the provider version

--- nodejs-hello-world/provider.tf
+++ nodejs-hello-world/provider.tf
@@ -2,9 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.64.0"
+      version = ">=3.0"
     }
   }
+  required_version = ">= 1.3.0"
 }
 
 provider "azurerm" {
