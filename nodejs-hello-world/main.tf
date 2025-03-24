85549d3e5e3eb36c3e55ab9286289dcee26bd7f8
jiyansh saini <rahulhbc@yahoo.in>
2025-03-24 22:04:29 +0530

    updated Ubuntu server name

--- nodejs-hello-world/main.tf
+++ nodejs-hello-world/main.tf
@@ -36,8 +36,8 @@ resource "azurerm_linux_virtual_machine" "frontend_vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22.04-lts-gen2"
-    version   = "22.04.202402140"
+    sku       = "19_04-gen2"
+    version   = "19.04.202001220"
   }
 
   provisioner "remote-exec" {
