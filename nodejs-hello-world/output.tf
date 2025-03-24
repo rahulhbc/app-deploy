25aa1ceda23795560cd05305234aa69e6f816597
jiyansh saini <rahulhbc@yahoo.in>
2025-03-24 20:24:13 +0530

    updated output.tf

--- nodejs-hello-world/output.tf
+++ nodejs-hello-world/output.tf
@@ -1,29 +1,29 @@
 output "public_ip" {
   description = "Public IP address of the VM"
-  value       = data.terraform_remote_state.networking.outputs.public_ip
+  value       = data.terraform_remote_state.network.outputs.public_ip
 }
 
 output "resource_group_name" {
   description = "The name of the existing resource group"
-  value       = data.terraform_remote_state.networking.outputs.resource_group_name
+  value       = data.terraform_remote_state.network.outputs.resource_group_name
 }
 
 output "location" {
   description = "The location of the existing resource group"
-  value       = data.terraform_remote_state.networking.outputs.location
+  value       = data.terraform_remote_state.network.outputs.location
 }
 
 output "vnet_name" {
   description = "The name of the existing Virtual Network"
-  value       = data.terraform_remote_state.networking.outputs.vnet_name
+  value       = data.terraform_remote_state.network.outputs.vnet_name
 }
 
 output "nsg_name" {
   description = "The name of the existing Network Security Group"
-  value       = data.terraform_remote_state.networking.outputs.nsg_name
+  value       = data.terraform_remote_state.network.outputs.nsg_name
 }
 
 output "frontend_nic" {
   description = "Network Interface ID from Network State"
-  value       = data.terraform_remote_state.networking.outputs.frontend_nic
+  value       = data.terraform_remote_state.network.outputs.frontend_nic
 }
