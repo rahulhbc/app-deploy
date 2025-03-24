315cca2b07a2b639751913f4a5e3896f20168b85
jiyansh saini <rahulhbc@yahoo.in>
2025-03-25 03:48:37 +0530

    updated user data script

--- nodejs-hello-world/userdata.sh
+++ nodejs-hello-world/userdata.sh
@@ -10,9 +10,13 @@ sudo apt-get install -y apt-transport-https ca-certificates curl software-proper
 
 # Add Docker repository and install Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update -y
-sudo apt-get install -y docker-ce
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu disco stable"
+sudo apt-get update -y 
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io 
+sudo systemctl start docker 
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+
 
 # Start Docker service
 sudo systemctl start docker
