3a1ecd5483198ad51fdb3be635b12152f7b3ac3e
jiyansh saini <rahulhbc@yahoo.in>
2025-03-24 12:07:18 +0530

    updated variable.tf

--- nodejs-hello-world/variables.tf
+++ nodejs-hello-world/variables.tf
@@ -1,9 +1,9 @@
 variable "resource_group_name" {
   description = "Resource group name"
-  default     = module.network.resource_group_name
+#  default     = module.network.resource_group_name
 }
 
 variable "location" {
   description = "Azure region"
-  default     = module.network.location
+#  default     = module.network.location
 }
