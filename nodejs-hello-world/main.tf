0dae1bb7f5591a784fbe4cae0b375b04f5653616
jiyansh saini <rahulhbc@yahoo.in>
2025-03-24 20:18:46 +0530

    Removed unsupported resources from main.tf

--- nodejs-hello-world/main.tf
+++ nodejs-hello-world/main.tf
@@ -4,7 +4,7 @@ data "terraform_remote_state" "networking" {
 
   config = {
     resource_group_name  = "iac-secure-rg"
-    storage_account_name = "app-deploy"
+    storage_account_name = "appdeploy001"
     container_name       = "tfstate"
     key                  = "3TierIaC.tfstate"
   }
