0f83453d00e8282626acfe9d8cd5749af720c4f7
jiyansh saini <rahulhbc@yahoo.in>
2025-03-24 13:51:48 +0530

    Fixing environment issues

--- nodejs-hello-world/backend.tf
+++ nodejs-hello-world/backend.tf
@@ -4,6 +4,6 @@ terraform {
     storage_account_name = "storagelab001"
     container_name       = "3tier-app"
     key                  = "app-deploy.tfstate"
-    subscription_id      = "7c63e1b6-0a9a-487e-b657-e58721d3e957"
+#   subscription_id      = "7c63e1b6-0a9a-487e-b657-e58721d3e957"
   }
 }
