88a7bf7dc80f1a7086c404c73a35c44ab9b8dc89
jiyansh saini <rahulhbc@yahoo.in>
2025-03-24 14:44:12 +0530

    Fix: Update GitHub Actions to use environment variables

--- nodejs-hello-world/variables.tf
+++ nodejs-hello-world/variables.tf
@@ -1,8 +1,18 @@
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
-variable "subscription_id" {}
+variable "client_id" {
+  description = "Azure Client ID"
+}
 
+variable "client_secret" {
+  description = "Azure Client Secret"
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+}
 variable "resource_group_name" {
   description = "Resource group name"
   default     = "iac-secure-rg"
